8e83780612b1d1aa23fb4d4bb6ffec658b40d395
matthuntsberry <Matthew.Marshall.Huntsberry@ibm.com>
2019-01-14 14:21:50 -0600

    First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: "es5",
+  "useTabs": true,      // Indent lines with tabs instead of spaces.
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true
+};  
