5be55ddb9fea46457ccb3f434a5b920e34136915
matthuntsberry <Matthew.Marshall.Huntsberry@ibm.com>
2019-01-25 10:01:17 -0600

    Created filters for mobile view
    Added in styling for the filters section for mobile view only.  Added
    in SVG chevron and changed default stying to match the psd file.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="app">
     <global-header />
     <hero-main />
+    <filters />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Hero from './components/Hero-main.vue';
+import Filters from './components/Filters.vue';
 
 export default {
 	name: 'app',
 	components: {
 		'global-header': Header,
 		'hero-main': Hero,
+		filters: Filters,
 	},
 };
 </script>
---
+++ src/components/Filters.vue
@@ -0,0 +1,44 @@
+<template>
+    <form class="filters u-flex" role="form">
+      <select name="make" class="filters-list" role="menu">
+        <optgroup label="Make" role="group">
+          <option value="option1" class="filters-list__item" role="group">Make</option>
+          <option value="option1" class="filters-list__item" role="group">Option 1</option>
+          <option value="option2" class="filters-list__item" role="group">Option 2</option>
+          <option value="option3" class="filters-list__item" role="group">Option 3</option>
+          <option value="option4" class="filters-list__item" role="group">Option 4</option>
+        </optgroup>
+      </select>
+      <select name="model" class="filters-list" role="menu">
+        <optgroup label="Model">
+          <option value="option1" class="filters-list__item" role="group">Model</option>
+          <option value="option1" class="filters-list__item" role="group">Option 1</option>
+          <option value="option2" class="filters-list__item" role="group">Option 2</option>
+          <option value="option3" class="filters-list__item" role="group">Option 3</option>
+          <option value="option4" class="filters-list__item" role="group">Option 4</option>
+        </optgroup>
+      </select>
+      <select name="model" class="filters-list" role="menu">
+        <optgroup label="Price">
+          <option value="option1" class="filters-list__item" role="group">Price</option>
+          <option value="option1" class="filters-list__item" role="group">Option 1</option>
+          <option value="option2" class="filters-list__item" role="group">Option 2</option>
+          <option value="option3" class="filters-list__item" role="group">Option 3</option>
+          <option value="option4" class="filters-list__item" role="group">Option 4</option>
+        </optgroup>
+      </select>
+       <input type="submit" class="filters__button" value="Find My Dream Car">
+    </form>
+</template>
+
+<script>
+export default {
+	name: 'filters',
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss">
+</style>
